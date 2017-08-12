c524873346196930da56f8bbbb8c224b2983a265
crzidea <i@crzidea.com>
2017-08-12 14:19:12 +0800

    Add button to transfer to put.io

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+class Token {
+  constructor(string) {
+    const [_, service, dialect] = string.match(/^(.+):(.+)$/)
+    const token = this[service](dialect)
+    localStorage[`token_${service}`] = token
+  }
+  ['put.io'](dialect) {
+    const [_, $1, token] = dialect.match(/^(.+)=(.+)$/)
+    return token
+  }
+}
+
 (() => {
+
+  const query = queryInHash()
+  if (query.token) {
+    new Token(query.token)
+    delete query.token
+    const querystring = Qs.stringify(query)
+    location.hash = `#/?${querystring}`
+  }
 
   const now = new Date
   const loading = window.results.innerHTML
@@ -8,7 +28,6 @@
     { terms: { extnames: format } },
     { match: { name: advises } },
   ]
-  const qs = window.Qs
 
   nunjucks.configure({
     web: { async: true, useCache: true },
@@ -17,19 +36,7 @@
 
   async function refetch() {
     window.results.innerHTML = loading
-    let q
-    {
-      const search = location.hash.slice(location.hash.indexOf('?') + 1)
-      q = qs.parse(search).q
-      if (!q) {
-        q = qs.parse(location.search.slice(1)).q
-      }
-      if (q) {
-        window.search.value = q
-      } else {
-        q = 'Movie'
-      }
-    }
+    let q = findQ()
     document.title = `${q} - Zhongzi.io`
 
     ga('set', 'page', `${location.pathname}${location.search}${location.hash}`);
@@ -46,7 +53,7 @@
         }
       }
       const _q = b64EncodeUnicode(JSON.stringify(search))
-      const response = await fetch(`https://api.zhongzi.io/search/?${qs.stringify({q: _q})}`)
+      const response = await fetch(`https://api.zhongzi.io/search/?${Qs.stringify({q: _q})}`)
       const results = await response.json()
 
       nunjucks.render('template.html', results, (error, html) => {
@@ -65,6 +72,24 @@
   window.addEventListener("hashchange", refetch)
 
 })()
+
+function queryInHash() {
+  return Qs.parse(location.hash.slice(location.hash.indexOf('?') + 1))
+}
+
+function findQ() {
+  let q
+  q = queryInHash().q
+  if (!q) {
+    q = Qs.parse(location.search.slice(1)).q
+  }
+  if (q) {
+    window.search.value = q
+  } else {
+    q = 'Movie'
+  }
+  return q
+}
 
 const isiOSDevice = navigator.userAgent.match(/ipad|iphone/i);
 
@@ -99,6 +124,82 @@ function magnet(element, event) {
   }
   document.execCommand('copy')
   notie.alert({text: 'Magnet has been copied to clipboard!'})
+
+}
+
+async function watch(element) {
+
+  const API_PREFIX = 'https://api.put.io/v2'
+  const token = localStorage['token_put.io']
+  if (!token) {
+    const redirect_uri_querystring = Qs.stringify({
+      q: findQ(),
+      token: 'put.io:'
+    })
+    const querystring = Qs.stringify({
+      client_id: 2999,
+      response_type: 'token',
+      redirect_uri: `${location.origin}${location.pathname}#/?${redirect_uri_querystring}`
+    })
+    location.href = `https://api.put.io/v2/oauth2/authenticate?${querystring}`
+    return
+  }
+
+  element.innerText = 'Transferring'
+  element.disabled  = true
+
+  const API_SUFFIX = `&oauth_token=${token}`
+  const {infohash, magnet} = element.parentNode.dataset
+  const files = await fetch(
+    `${API_PREFIX}/files/list?${API_SUFFIX}`
+  )
+  .then(response => response.json())
+  .then(json => json.files)
+
+  //let directory = list.files.find((file) => infohash === file.name)
+  let root = files.find((file) => 'zhongzi.io' === file.name)
+  if (!root) {
+    root = await fetch(
+      `${API_PREFIX}/files/create-folder?${API_SUFFIX}`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+          'accept'      : 'application/json'
+        },
+        body: JSON.stringify({name: 'zhongzi.io'})
+      }
+    )
+    .then(response => response.json())
+    .then(json => json.file)
+  }
+
+  let transfer = await fetch(
+    `${API_PREFIX}/transfers/add?${API_SUFFIX}`,
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'content-type': 'application/json',
+        'accept'      : 'application/json'
+      },
+      body: JSON.stringify({url: magnet, save_parent_id: root.id})
+    }
+  )
+  .then(response => response.json())
+  .then(json => json.transfer)
+
+  while (!transfer.finished_at) {
+    transfer = await fetch(
+      `${API_PREFIX}/transfers/${transfer.id}?${API_SUFFIX}`
+    )
+    .then(response => response.json())
+    .then(json => json.transfer)
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+  }
+
+  element.value = 'Watch Online'
 
 }
 
